413afd1f9fec32fea36c975dffa9c7628dc651ad
Aaron Murillon <abreeze311@gmail.com>
2025-01-07 19:06:49 +0800

    Added tileMap2DArray to make it easier loading a bunch of tiles

--- src/tile/TileManager.java
+++ src/tile/TileManager.java
@@ -4,6 +4,7 @@ import main.GamePanel;
 
 import javax.imageio.ImageIO;
 import java.awt.*;
+import java.awt.image.BufferedImage;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
@@ -13,13 +14,15 @@ public class TileManager {
 
     GamePanel gamePanel;
     Tile[] tiles;
-    int mapTileNum[][];
+    int[][] mapTileNum;
+    Tile[][] tileMap2DArray;
 
     public TileManager(GamePanel gamePanel){
 
         this.gamePanel = gamePanel;
 
         tiles = new Tile[10];
+        tileMap2DArray = new Tile[50][50];
         mapTileNum = new int[gamePanel.maxScreenCol][gamePanel.maxScreenRow];
 
         getTileImage();
@@ -39,15 +42,48 @@ public class TileManager {
 
             tiles [1].image = ImageIO.read(getClass().getResourceAsStream("/tiles/summer_tiles/plant_tiles/plant_1.png"));
 
+            // The column in tileMap2DArray[col][row] is the type of tile e.g. first_grass_, plant_ .  Row in tileMap2DArray[col][row] is tile's variation or parts since tiles are chopped into 16x16 e.g. first_grass_1, first_grass_2, plant_1, plant_2.
+
+            // tileMap2DArray[0][x] is first_grass_1, 2 3, 4... 9.png
+            BufferedImage[] tileImages = loadTileArray("/tiles/summer_tiles/ground_grass_tiles/first_grass_", 9);
+            for (int i = 0; i < tileImages.length; i++) {
+
+                tileMap2DArray[0][i] = new Tile();
+                tileMap2DArray[0][i].image = tileImages[i];
+
+            }
+
+
+
+
+
+
+
         }catch(IOException e){
             e.printStackTrace();
         }
 
     }
 
+    public BufferedImage[] loadTileArray(String basePath, int count) throws IOException{
+
+        BufferedImage[] tile = new BufferedImage[count];
+        for(int i = 0; i < count; i++){
+
+            tile[i] = ImageIO.read(getClass().getResourceAsStream(basePath + (i + 1) + ".png"));
+
+        }
+
+        return tile;
+
+    }
+
+
+
     public void loadMap(){
 
         try{
+
             // Import text file
             InputStream inputStream = getClass().getResourceAsStream("/maps/MapData.txt");
             // Read the text file
@@ -66,13 +102,16 @@ public class TileManager {
                 while(col < gamePanel.maxScreenCol){
 
                     // Splits the numbers with space
-                    String numbers[] = line.split(" ");
+                    String[] numbers = line.split(" ");
 
                     // Converts the elements inside the numbers[] array string into integer
                     int num = Integer.parseInt(numbers[col]);
 
-                    // Put the converted integer into the mapTileNum[][] 2D array. Basically, this will look like the MapData.txt but, instead of String, it's 2D array, so every numbers inside MapData.txt can be read and access the same way it was arranged.
+                    // Put the converted integer into mapTileNum[][] 2D array. Basically, this will look like the MapData.txt but, instead of String, it's 2D array, so every numbers inside MapData.txt can be read and access the same way it was arranged.
                     mapTileNum[col][row] = num;
+
+
+
                     col++;
                 }
                 if(col == gamePanel.maxScreenCol){
@@ -106,7 +145,7 @@ public class TileManager {
 
             int tileNum = mapTileNum[col][row];
 
-            graphics2D.drawImage(tiles[0].image, x, y, gamePanel.tileSize, gamePanel.tileSize, null);
+            graphics2D.drawImage(tileMap2DArray[0][4].image, x, y, gamePanel.tileSize, gamePanel.tileSize, null);
             // Since the grass plant is foreground, the size is 16 x 16 instead of 48 x 48
             graphics2D.drawImage(tiles[tileNum].image, x, y, 16, 16, null);
 
